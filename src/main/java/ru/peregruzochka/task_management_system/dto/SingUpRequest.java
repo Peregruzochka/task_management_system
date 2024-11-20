0adaf6476f3b4a4861896411667a69a5c7db0b7a
Peregruzochka <86581077+Peregruzochka@users.noreply.github.com>
2024-11-20 18:17:14 +0300

    Настройка документации

--- src/main/java/ru/peregruzochka/task_management_system/dto/SingUpRequest.java
+++ src/main/java/ru/peregruzochka/task_management_system/dto/SingUpRequest.java
@@ -1,5 +1,6 @@
 package ru.peregruzochka.task_management_system.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,17 +9,26 @@ import lombok.Setter;
 import lombok.ToString;
 
 
+@ToString
 @Setter
 @Getter
-@ToString
+@Schema(description = "Request object for user registration")
 public class SingUpRequest {
+
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Invalid email format")
+    @Schema(description = "User's email address", example = "user@example.com")
     private String email;
 
-    @Pattern(regexp ="^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d).*$", message = "The password must contain at least 8 characters, including at least one letter and one number")
+    @Pattern(
+            regexp = "^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d).*$",
+            message = "The password must contain at least 8 characters, including at least one letter and one number"
+    )
+    @Schema(description = "User's password (minimum 8 characters, must include at least one letter and one number)",
+            example = "Password123")
     private String password;
 
     @NotBlank(message = "Username cannot be empty")
+    @Schema(description = "Username for the new account", example = "john_doe")
     private String username;
 }
