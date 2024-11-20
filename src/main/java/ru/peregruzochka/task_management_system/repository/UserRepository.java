3af931d7d2ca785344838870e0d2e13b8b7e5542
Peregruzochka <86581077+Peregruzochka@users.noreply.github.com>
2024-11-20 18:22:04 +0300

    начал менять фичу

--- src/main/java/ru/peregruzochka/task_management_system/repository/UserRepository.java
+++ src/main/java/ru/peregruzochka/task_management_system/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.peregruzochka.task_management_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.peregruzochka.task_management_system.entity.User;
 
@@ -13,4 +14,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
+
+    @Query("""
+        SELECT u FROM User u
+        WHERE u.role = 'ROLE_ADMIN'
+        """)
+    User getAdmin();
 }
--- src/main/java/ru/peregruzochka/task_management_system/service/TaskService.java
+++ src/main/java/ru/peregruzochka/task_management_system/service/TaskService.java
@@ -8,12 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.peregruzochka.task_management_system.entity.Task;
 import ru.peregruzochka.task_management_system.entity.TaskStatus;
 import ru.peregruzochka.task_management_system.entity.User;
+import ru.peregruzochka.task_management_system.entity.UserRole;
 import ru.peregruzochka.task_management_system.repository.UserRepository;
 
 import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 import static ru.peregruzochka.task_management_system.entity.TaskPriority.LOW;
 import static ru.peregruzochka.task_management_system.entity.TaskStatus.TODO;
@@ -28,28 +28,34 @@ public class TaskService {
 
     @Transactional
     public Task createTask(Task task) {
+        User admin = userRepository.getAdmin();
+
+        task.setAuthor(defaultIfNull(task.getAuthor(), admin));
+        task.setAssignee(defaultIfNull(task.getAssignee(), admin));
+
         validateUser(task.getAuthor(), "author");
         validateUser(task.getAssignee(), "assignee");
-        if (Objects.isNull(task.getPriority())) {
-            task.setPriority(LOW);
-        }
-        if (Objects.isNull(task.getStatus())) {
-            task.setStatus(TODO);
-        }
+
+        task.setPriority(defaultIfNull(task.getPriority(), LOW));
+        task.setStatus(defaultIfNull(task.getStatus(), TODO));
+
         Task createdTask = taskRepository.save(task);
         log.info("Task created: {}", createdTask);
+
         return createdTask;
     }
+
+
 
     @Transactional
     public Task updateTask(Task task) {
         Task taskToUpdate = getTaskById(task.getId());
         validateAuthor(task, taskToUpdate);
 
-        updateField(task::getTitle, taskToUpdate::setTitle);
-        updateField(task::getDescription, taskToUpdate::setDescription);
-        updateField(task::getPriority, taskToUpdate::setPriority);
-        updateField(task::getStatus, taskToUpdate::setStatus);
+        updateField(taskToUpdate::setTitle, task.getTitle());
+        updateField(taskToUpdate::setDescription, task.getDescription());
+        updateField(taskToUpdate::setPriority, task.getPriority());
+        updateField(taskToUpdate::setStatus, task.getStatus());
 
         if (Objects.nonNull(task.getAssignee())) {
             validateAssigneeExists(task.getAssignee().getId());
@@ -95,15 +101,18 @@ public class TaskService {
         }
     }
 
-    private <T> void updateField(Supplier<T> getter, Consumer<T> setter) {
-        if (Objects.nonNull(getter.get())) {
-            T value = getter.get();
+    private <T> void updateField(Consumer<T> setter, T value) {
+        if (Objects.nonNull(value)) {
             setter.accept(value);
         }
     }
 
+    private <T> T defaultIfNull(T value, T defaultValue) {
+        return Objects.isNull(value) ? defaultValue : value;
+    }
+
     private void validateUser(User user, String role) {
-        if (!userRepository.existsById(user.getId())) {
+        if (!user.getRole().equals(UserRole.ROLE_ADMIN) && !userRepository.existsById(user.getId())) {
             log.error("This user cannot be the {} because they do not exist. Invalid user id = {}", role, user.getId());
             throw new IllegalArgumentException(String.format("This user cannot be the %s because they do not exist", role));
         }
