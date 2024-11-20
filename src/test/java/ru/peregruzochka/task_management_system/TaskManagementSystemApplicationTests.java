7d2d296e125b0d4f6ed2bf9e482137d7299d0046
Peregruzochka <peregruzochka48@mail.ru>
2024-11-21 02:25:11 +0300

    настроил Dockerfile и docker-compose.yaml

--- src/test/java/ru/peregruzochka/task_management_system/TaskManagementSystemApplicationTests.java
+++ src/test/java/ru/peregruzochka/task_management_system/TaskManagementSystemApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.peregruzochka.task_management_system;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class TaskManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER =
+            new PostgreSQLContainer<>("postgres:13.3")
+                    .withDatabaseName("testdb")
+                    .withUsername("testuser")
+                    .withPassword("testpassword");
 
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("spring.datasource.url", POSTGRESQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRESQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRESQL_CONTAINER.getPassword());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
