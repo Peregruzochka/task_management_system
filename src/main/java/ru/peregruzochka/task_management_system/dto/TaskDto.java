39fb1f5a6913b608ad0d9cfefd279eb16bc75c09
Peregruzochka <peregruzochka48@mail.ru>
2024-11-20 22:22:12 +0300

    some fix

--- src/main/java/ru/peregruzochka/task_management_system/dto/TaskDto.java
+++ src/main/java/ru/peregruzochka/task_management_system/dto/TaskDto.java
@@ -2,7 +2,11 @@ package ru.peregruzochka.task_management_system.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.peregruzochka.task_management_system.entity.TaskPriority;
 import ru.peregruzochka.task_management_system.entity.TaskStatus;
 
